3f4e73d65fb7bc0f6707bce94e9c052e901d32b5
Matthew Collier <collier.matthew@gmail.com>
2019-02-19 12:31:43 -0500

    Add core files.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# base64url-universal ChangeLog
+
+## 1.0.0 - TBD
+
+- See git history.
---
+++ tests/api.spec.js
@@ -0,0 +1,44 @@
+/*!
+ * Copyright (c) 2018-2019 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const chai = require('chai');
+const env = require('../lib/env');
+chai.should();
+
+if(env.nodejs) {
+  global.TextEncoder = require('util').TextEncoder;
+}
+
+const {encode, decode} = require('..');
+const testString = 'The quick brown fox jumps over the lazy dog.';
+const testEncoding =
+  'VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZy4';
+
+describe('base64url-universal APIs', () => {
+  describe('encode', () => {
+    it('should properly encode data', async () => {
+      const result = encode(_strToUint8Array(testString));
+      result.should.equal(testEncoding);
+    });
+  }); // end encode
+
+  describe('decode', () => {
+    it('should properly decode data', async () => {
+      const result = decode(testEncoding);
+      result.should.equal(testString);
+    });
+  }); // end decode
+});
+
+function _strToUint8Array(data) {
+  if(typeof data === 'string') {
+    // convert data to Uint8Array
+    return new TextEncoder().encode(data);
+  }
+  if(!(data instanceof Uint8Array)) {
+    throw new TypeError('"data" be a string or Uint8Array.');
+  }
+  return data;
+}
